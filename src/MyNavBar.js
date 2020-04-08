1585ef84f1e9b67ead22ac99f6250478ab8296f0
wzamites <bj_zamites@comcast.net>
2020-04-08 05:40:43 -0400

    change ribbon logo to mcmocf logo

--- src/MyNavBar.js
+++ src/MyNavBar.js
@@ -6,12 +6,12 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function MyNavBar() {
   return (
-    <Navbar bg="light" expand="md" sticky="top">
+    <Navbar collapseOnSelect bg="light" expand="lg" sticky="top">
     <Container>
-      <Navbar.Brand href="#home">
+      <Navbar.Brand href="/">
       <img
-      src="/favicon.svg"
-      height="30"
+      src="/MCMOCF_Logo.png"
+      height="60"
       className="d-inline-block align-top"
       alt="React Bootstrap logo"
       />
@@ -21,8 +21,8 @@ function MyNavBar() {
         <Nav className="mr-auto">
           <Nav.Link href="#about">About</Nav.Link>
           <Nav.Link href="#golf">Golf Outing</Nav.Link>
-          <Nav.Link href="#scholarship">Scholarship Fund</Nav.Link>
-          <Nav.Link href="#research">Research Grants</Nav.Link>
+          <Nav.Link href="#fundraising">Fundraising</Nav.Link>
+          <Nav.Link href="#contact">Contact Us</Nav.Link>
         </Nav>
         <Nav>
           <Nav.Link href="https://www.facebook.com/mcmocf/" target="_blank">
