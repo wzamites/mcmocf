a559207bf421d3816da95b83e624f053bbd1e14f
wzamites <bj_zamites@comcast.net>
2020-04-08 08:09:06 -0400

    change date

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ function Header() {
     <header id="home" className="bigBackground App-header ">
       <div className="contentContainer centerText shadowText">
         <h1>13<sup>th</sup> Annual Charity Golf Outing</h1>
-        <h2>Friday, October 19<sup>th</sup>, 2020</h2>
+        <h2>Friday, October 23<sup>rd</sup>, 2020</h2>
       </div>
       <p>
         <Button variant="light">Register</Button>{' '}
