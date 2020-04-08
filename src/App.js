5f1e2801e3fada533a34b94c9c0749677f042ed5
wzamites <bj_zamites@comcast.net>
2020-04-08 05:44:52 -0400

    add about page and footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Footer from './Footer'
 import './App.css';
 
 import MyNavBar from './MyNavBar.js'
 import HeaderCarousel from './HeaderCarousel'
+import About from './About'
 
 function App() {
   return (
     <div>
       <MyNavBar />
       <HeaderCarousel />
+      <About />
+      <Footer />
     </div>
   );
 }
