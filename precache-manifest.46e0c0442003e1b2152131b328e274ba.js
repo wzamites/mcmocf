f2cb2dabb553485dd4480bfeca2d11903f920fe8
wzamites <bj_zamites@comcast.net>
2020-04-08 09:24:42 -0400

    Updates

---
+++ precache-manifest.46e0c0442003e1b2152131b328e274ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895e6a8976223200670c84ca93d332f0",
+    "url": "/mcmocf/index.html"
+  },
+  {
+    "revision": "d31e69ab9b2f2698addd",
+    "url": "/mcmocf/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b7587c72f77df2ee28fa",
+    "url": "/mcmocf/static/css/main.1187fa96.chunk.css"
+  },
+  {
+    "revision": "d31e69ab9b2f2698addd",
+    "url": "/mcmocf/static/js/2.3d54be9f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mcmocf/static/js/2.3d54be9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7587c72f77df2ee28fa",
+    "url": "/mcmocf/static/js/main.9878eac5.chunk.js"
+  },
+  {
+    "revision": "45bd756c279c8b176776",
+    "url": "/mcmocf/static/js/runtime-main.98973fe9.js"
+  }
+]);
