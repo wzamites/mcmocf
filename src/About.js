b8c591a7e92683e3175798869ff14642c51e3ed1
wzamites <bj_zamites@comcast.net>
2020-04-26 18:58:54 -0400

    change space error

--- src/About.js
+++ src/About.js
@@ -9,37 +9,32 @@ import michele from './img/Michele-Photo.jpg'
 function About() {
   return (
     <Container id="about" className="centerText pinkText">
-    <br />
-    <br />
-    <br />
-          <h2>
-          Our mission is to raise money for the causes
-closest to Michele’s heart—
-</h2><h5>raising awareness and fundraising for Ovarian Cancer research, and supporting students
-with financial need.
-          </h5>
-          <br />
-          <br />
-          <Row>
-          <Col xs={12} md={6}>
-          <Image src={michele} className="michelePhoto" roundedCircle />
-          </Col>
-          <Col xs={12} md={6}>
-          <br />
-          <br />
-          <br />
-          <p>MCMOCF was founded in honor of Michele C. McLennan
-          who battled Ovarian Cancer until 2005. We are a family
-          founded and operated, non-profit foundation. <a href="#">Learn more about Michele's story...</a>
-          </p>
-          </Col>
-          </Row>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
+      <br />
+      <br />
+      <br />
+      <h2>Our mission is to raise money for the causes closest to Michele’s heart</h2>
+      <h5>raising awareness and fundraising for Ovarian Cancer research, and supporting students with financial need</h5>
+      <br />
+      <br />
+      <Row>
 
+        <Col xs={12} md={6}>
+        <Image src={michele} className="michelePhoto" roundedCircle />
+        </Col>
+
+        <Col xs={12} md={6}>
+        <br />
+        <br />
+        <br />
+        <p>MCMOCF was founded in honor of Michele C. McLennan who battled Ovarian Cancer until 2005. We are a family founded and operated, non-profit foundation. <a href="#">Learn more about Michele's story...</a></p>
+        </Col>
+
+      </Row>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </Container>
   );
 }
